91b3ee4ed8a3a60fb6dcf8f0b9be0e79aa55f1c8
Anubhav Jain <87580533+jAnubhav@users.noreply.github.com>
2025-03-30 23:49:55 +0530

    Delete blockchain directory

--- blockchain/data.py
+++
@@ -1,5 +0,0 @@
-rest_url = "https://api.devnet.aptoslabs.com/v1"
-faucet_url = "https://faucet.devnet.aptoslabs.com"
-
-man_dir, man_subdir = "./contracts/manager/", "./build/manager/"
-cli_dir, cli_subdir = "./contracts/client/", "./build/client/"
