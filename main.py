89179dc9877a28f3a6757bc2a0641e72b2769777
Anubhav Jain <jainanubhav2003@gmail.com>
2025-04-06 00:56:52 +0530

    completed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-
-import asyncio as asy
+from quart import Quart, request, jsonify
+from quart_cors import cors
 
 from controller import assign_account, get_data, add_data
 
@@ -10,20 +8,24 @@ from private_data import secret_key
 
 from database_handler import *
 
-app = Flask("Medicords"); CORS(app)
-app.config['SECRET_KEY'] = secret_key
+
+
+### Quart App
+
+app = Quart("Medicords"); cors(app)
+app.secret_key = secret_key
 
 
 
 ### Routes
 
 @app.route("/login", methods=["POST"])
-def login():
+async def login():
     '''
     This API will handle Login Requests
     '''
 
-    data = request.get_json(); password = data["password"]
+    data = await request.get_json(); password = data["password"]
     client_id = data["aadharId"] + data["nationalId"]
 
     user = retrieve(client_id)
@@ -34,35 +36,35 @@ def login():
     return generate_token(secret_key, client_id)
 
 @app.route("/signup", methods=["POST"])
-def signup():
+async def signup():
     '''
     This API will handle Signup Requests
     '''
 
-    data = request.get_json()
+    data = await request.get_json()
     client_id = data["aadharId"] + data["nationalId"]
 
     user = retrieve(client_id)
 
     if (user): return "Failure"
-    asy.run(assign_account(client_id))
+    await assign_account(client_id)
 
     add(client_id, data["name"], data["password"])
     
     return generate_token(secret_key, client_id)
 
 @app.route("/get-data", methods=["POST"])
-def get_user_data():
+async def get_user_data():
     '''
     This API will decode the token and return data
     '''
 
-    cli_id = decode_token(secret_key, request.get_json()["token"])
+    cli_id = decode_token(secret_key, (await request.get_json())["token"])
     key, user = "aadharId" if (len(cli_id) == 12) else "nationalId", retrieve(cli_id)
 
     if (not user): return jsonify({"cred": "Failure"})
     
-    records = asy.run(get_data(cli_id))
+    records = await get_data(cli_id)
     return jsonify({"cred": get(key, cli_id), "records": records})
 
 
@@ -70,13 +72,12 @@ def get_user_data():
 ### Hospital Functions adding records
 
 @app.route("/add-record", methods=["POST"])
-def add_record():
-    data = request.get_json()
-    
+async def add_record():
+    data = await request.get_json()
     user = retrieve(data["client_id"])
 
     if (not user): return "AadharId"
-    date = asy.run(add_data(**data))
+    date = await add_data(**data)
 
     return date
 
